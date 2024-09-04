955a294dfc8395fb85be47d3bf1c6bd283a3685a
Jose <125104534+jmtx1020@users.noreply.github.com>
2024-09-03 22:35:59 -0600

    add winrm support (#12)
    * added support for winrm
    
    * bump version v0.3.0
    
    * made linter happy
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,6 @@
 
 require 'kitchen/provisioner/yansible_pusher'
 
-# Load all integration test files
-Dir[File.expand_path("../integration/**/*_spec.rb", __FILE__)].each { |f| require f }
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -12,6 +9,8 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
   config.expose_dsl_globally = true
+
+  config.filter_run_excluding winrm: true # use this to test on non windows systems
 
 
   config.expect_with :rspec do |c|
